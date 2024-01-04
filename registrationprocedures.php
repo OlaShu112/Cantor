78c81b228f36aed234030a0c4433c300b37d31ed
Oladotun <Oladotun.A.Ogundari@student.shu.ac.uk>
2024-01-04 03:52:35 +0000

    Created

---
+++ registrationprocedures.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" media="screen and (max-width: 600px)" href="css/mobile.css">
+    <script src="components/index.js"></script>
+    <title>Registration Procedures - Cantor College</title>
+</head>
+
+<body>
+    <?php include 'components/header.php'; ?>
+    <section id="information-for-staff" class="hero">
+        <div class="container">
+        <h2>Registration Procedures</h2>
+
+        </div>
+        </section>
+    <main>
+        <section id="registration-procedures">
+    <h3>Registration Procedures</h3>
+    <ol>
+        <li>Confirm your eligibility for membership in Cantor College Learning Resources. Membership is typically extended to all students and staff of the College. However, individuals from external organizations may also apply, subject to certain conditions.</li>
+        <li>Submit a formal application to become a member. The application form is available at the Learning Resources reception or on our official website.</li>
+        <li>As part of the registration process, all applicants are required to sign an undertaking. This includes a commitment to abide by the rules and regulations set forth by Cantor College Learning Resources.</li>
+        <li>While membership is complimentary for students and staff, there may be applicable fees for individuals from external organizations. Details regarding fees and payment methods can be obtained during the application process.</li>
+        <li>Upon successful application, members will be issued unique identification (ID) cards. These cards are non-transferable, and the named individual is responsible for all items borrowed on it. Members must be prepared to show their ID cards when requested by College staff.</li>
+        <li>Non-members seeking temporary access must sign the Visitors' Book on each visit. Proper identification may be required, and access is limited to study purposes only.</li>
+        <li>Cantor College Learning Resources may admit individuals or organizations for study purposes, even if they are not eligible for full membership. Such cases are considered on an individual basis, and arrangements can be made by contacting our External Services department.</li>
+    </ol>
+    </section>
+        <!-- Other sections go here -->
+    </main>
+
+    <?php include 'components/footer.php'; ?>
+    
+</body>
+
+</html>
